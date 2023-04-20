e6568411c79fafd861e024d060398fb4abf835ea
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2023-04-20 10:56:20 -0300

    Add files via upload

--- 05_mesa.sh
+++ 05_mesa.sh
@@ -2,16 +2,19 @@
 
 sudo pacman -Sy --noconfirm archlinux-keyring
 
-yay -Sy directx-headers-git
+sudo pacman -Sy --noconfirm mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+sudo pacman -Sy --noconfirm libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
 
-sleep 5
+#yay -Sy directx-headers-git
 
-yay -Sy mesa-git
+#sleep 5
 
-sleep 5
+#yay -Sy mesa-git
 
-yay -Sy lib32-mesa-git
+#sleep 5
 
-sleep 5
+#yay -Sy lib32-mesa-git
 
-yay -Sy xf86-video-amdgpu-git
+#sleep 5
+
+#yay -Sy xf86-video-amdgpu-git
