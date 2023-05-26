998f3d816a651bec70391ef54cd9a1aca48faec9
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2023-05-26 18:57:24 -0300

    Add files via upload

--- 0A_apps.sh
+++ 0A_apps.sh
@@ -4,9 +4,9 @@ yay -Sy --noconfirm timeshift openrgb-git microsoft-edge-stable-bin teams steam-
 
 sudo pacman -Sy --noconfirm avidemux-qt gimp kdenlive remmina obs-studio darktable ddclient wine lutris telegram-desktop onlyoffice-bin
 
-sudo sed -i s/PIDFile=\/run/PIDFile=\/var\/run/g /usr/lib/systemd/system/ddclient.service
+sudo sed -i 's/PIDFile=\/run/PIDFile=\/var\/run/g' /usr/lib/systemd/system/ddclient.service
 
-sudo cp -v ~/Documents/LinuxCustomizations/ddclient.conf /etc/ddclient/ddclient.conf
+sudo cp -v /mnt/Backup/Documents/LinuxCustomizations/ddclient.conf /etc/ddclient/ddclient.conf
 
 sudo systemctl enable --now ddclient && sleep 3 && sudo systemctl restart ddclient && sleep 3 && sudo systemctl status ddclient
 
