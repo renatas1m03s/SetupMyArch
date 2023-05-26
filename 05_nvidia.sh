6ddfd9ddcfeec032e04a5fb98c16ee5c971783f2
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2023-05-26 18:55:32 -0300

    Add files via upload

--- 05_nvidia.sh
+++ 05_nvidia.sh
@@ -2,4 +2,6 @@
 
 sudo pacman -Sy --noconfirm archlinux-keyring
 
-pacman -Sy --noconfirm nvidia nvidia-zen nvidia-libgl nvidia-settings
+# sudo pacman -Sy --noconfirm nvidia nvidia-zen nvidia-libgl nvidia-settings
+
+sudo pacman -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia-libgl
